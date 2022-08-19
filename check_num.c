e26d57fc204ba58b1ae3c1416eb1068f22ca7e77
MedAliKrifa <daliolimona@gmail.com>
2022-08-19 04:53:00 +0100

    check_num.c

---
+++ check_num.c
@@ -0,0 +1,22 @@
+
+#include "monty.h"
+/**
+ * check_numb - checks if data is only composed of digits
+ * @value: string to check
+ * Return: 0 on failure or 1 on success
+ */
+int digits(char *value)
+{
+    unsigned int i;
+
+    if (value == NULL)
+        return (0);
+    for (i = 0; value[i] != '\0'; i++)
+    {
+        if (value[0] == '-' && i == 0)
+            continue;
+        if (isdigit(value[i]) == 0)
+            return (0);
+    }
+    return (1);
+}
